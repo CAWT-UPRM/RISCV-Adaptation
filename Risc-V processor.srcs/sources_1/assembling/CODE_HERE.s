31b043c57c218c80fe6a9acb535ac6c2968a09be
fernando.pizarro1 <fernando.pizarro1@upr.edu>
2025-07-02 09:39:36 -0400

    Final tweaks

--- Risc-V processor.srcs/sources_1/assembling/CODE_HERE.s
+++ Risc-V processor.srcs/sources_1/assembling/CODE_HERE.s
@@ -48,9 +48,8 @@ inner_loop:
     lw s5, 0(s4) # s5 = kernel[j]
     
     addi t5, t5, 1
-    # MAC!!!!!!!!!
-    # This should be changed to: 
-    #  MACF t4, s5, s3; ie t4 += s5 * s3
+    
+    # MAC
     mul t4, s5, s3
 
     blt t5, s7, inner_loop 
